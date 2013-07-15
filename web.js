fee0dca75acf85ab36b26f294f220760573aa082
Vahe Hovhannisyan <Vahefx@gmail.com>
2013-07-15 06:43:32 +0000

    Version 10

--- web.js
+++ web.js
@@ -2,9 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var buffer = new Buffer(100);
-buffer.write('fs.readFileSync('index.html',"utf-8")');
+buffer.write(fs.readFileSync('index.html',"utf-8"));
 app.get('/', function(request, response) {
-    response.send('buffer.toString("utf-8")');
+    response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
