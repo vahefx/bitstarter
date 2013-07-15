bafebe5ae63f7a065dc77b18470514f64ecf793c
Vahe Hovhannisyan <Vahefx@gmail.com>
2013-07-15 06:45:42 +0000

    Version 12

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var buffer = new Buffer(100);
-buffer.write(fs.readFileSync('index.html',"utf-8"));
+buffer.write(fs.readFileSync("index.html","utf-8"));
 app.get('/', function(request, response) {
     response.send(buffer.toString("utf-8"));
 });
