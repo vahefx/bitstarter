95d254e9621b6def50cfc0b68fae324897f19b40
Vahe Hovhannisyan <Vahefx@gmail.com>
2013-07-15 06:13:33 +0000

    version 6

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('buffer.write('fs.readFile(index.html)')');
+  response.send('buffer.write('fs.readFileSync('/home/ubuntu/hw/hw3/part1/bitstarter/index.html')')');
 });
 
 var port = process.env.PORT || 5000;
