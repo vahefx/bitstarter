32f71ac14effa9c1c306b7b86b59d7082080d1cd
Vahe Hovhannisyan <Vahefx@gmail.com>
2013-07-16 01:49:11 +0000

    New Version with HTML

--- web.js
+++ web.js
@@ -1,13 +1,8 @@
-var express = require('express');
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
 
-var app = express.createServer(express.logger());
-var buffer = new Buffer(100);
-buffer.write(fs.readFileSync("index.html","utf-8"));
-app.get('/', function(request, response) {
-    response.send(buffer.toString("utf-8"));
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(index);
+}).listen(9615);
